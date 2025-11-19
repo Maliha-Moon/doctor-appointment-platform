268f5374b6256a820c66b950376d14bada6ada29
Maliha-Moon <malihamoon05@gmail.com>
2025-11-19 06:21:40 +0600

    add doctors listing page & doctor dashboard APIs

--- app/(main)/doctors/[speciality]/page.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const SpecialityPage = async (params) => {
-  const { speciality } = await params;
-  return <div>SpecialityPage: {speciality}</div>;
-};
-
-export default SpecialityPage;
